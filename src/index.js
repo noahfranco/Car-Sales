958b4aff583240fe3330bc68b88e1cbc1072869b
noahfranco <noahfra@gmail.com>
2019-08-14 18:19:50 -0400

    set up reducer and inital state, set uo store and proider

--- src/index.js
+++ src/index.js
@@ -2,8 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Redux Imports 
+import { createStore } from 'redux';
+import { carReducer } from "./reducer/Reducer.js"; 
+import { Provider } from "react-redux"; 
+// Redux Imports
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+// Redux
+export const store = createStore(carReducer); 
+// Redux
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+
+// wrap provider around <App /> like I would wrap Router around <App /> 
+
+ReactDOM.render(
+    <Provider> 
+<App />
+    </Provider> 
+, rootElement);
